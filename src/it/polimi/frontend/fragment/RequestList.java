7df67897ea0aa7d7ae3f961e830c15e9b434b2cc
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-01-14 17:19:34 +0100

    rimosso retriever

--- src/it/polimi/frontend/fragment/RequestList.java
+++ src/it/polimi/frontend/fragment/RequestList.java
@@ -40,7 +40,7 @@ public class RequestList extends ListFragment{
 	/**
 	 * AsyncTask for calling Mobile Assistant API for checking into a place (e.g., a store)
 	 */
-	private class TestInsertTask extends AsyncTask<Void, Void, Void> {
+	private class TestInsertTask extends AsyncTask<Void, Void, Request> {
 
 		/**
 		 * Calls appropriate CloudEndpoint to indicate that user checked into a place.
@@ -48,7 +48,7 @@ public class RequestList extends ListFragment{
 		 * @param params the place where the user is checking in.
 		 */
 		@Override
-		protected Void doInBackground(Void... params) {
+		protected Request doInBackground(Void... params) {
 			Request req = new Request();
 			req.setDescription("Descrizione di prova");
 			req.setTitle("Titolo di prova");
@@ -63,82 +63,25 @@ public class RequestList extends ListFragment{
 					null);
 			reqBuilder = CloudEndpointUtils.updateBuilder(reqBuilder);
 			Requestendpoint reqEndpoint = reqBuilder.build();
+			Request r = null;
 			try {
-				reqEndpoint.insertRequest(req).execute();
+				r = reqEndpoint.insertRequest(req).execute();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			return null;
+			return r;
 		}
 		
 		@Override
-		protected void onPostExecute(Void result) {
-			new TestRetrieverTask().execute();
-		}
-	}
-
-	/**
-	 * AsyncTask for retrieving the list of places (e.g., stores) and updating the
-	 * corresponding results list.
-	 */
-	private class TestRetrieverTask extends AsyncTask<Void, Void, CollectionResponseRequest> {
-
-		@Override
-		protected CollectionResponseRequest doInBackground(Void... params) {
-
-
-			Requestendpoint.Builder endpointBuilder = new Requestendpoint.Builder(
-					AndroidHttp.newCompatibleTransport(), new JacksonFactory(), null);
-
-			endpointBuilder = CloudEndpointUtils.updateBuilder(endpointBuilder);
-
-
-			CollectionResponseRequest result;
-
-			Requestendpoint endpoint = endpointBuilder.build();
-
-			try {
-				result = endpoint.listRequest().execute();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				result = null;
-			}
-			return result;
-		}
-
-		@Override
-		protected void onPostExecute(CollectionResponseRequest result) {
-
-			/*
-			if (result == null || result.getItems() == null || result.getItems().size() < 1) {
-				if (result == null) {
-					resultsList.setText("Retrieving places failed.");
-				} else {
-					resultsList.setText("No places found.");
-				}
-
-				return;
-			}*/
-
-			List<Request> reqs = result.getItems();
-			if (reqs!=null){
+		protected void onPostExecute(Request result) {
+			if (result!=null){
 				ArrayList<String> reqTitles = new ArrayList<String>();
-				for (int i=0; i<reqs.size(); i++) {
-					reqTitles.add(reqs.get(i).getTitle());
-				}
+				reqTitles.add(result.getTitle());
 				ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, reqTitles); 
 				setListAdapter(adapter);
 			}
-			/*StringBuffer placesFound = new StringBuffer();
-			
-			for (Request place : reqs){
-				placesFound.append(place.getName() + "\r\n");
-			}
-
-			resultsList.setText(placesFound.toString());*/
-
 		}
 	}
+
 }
