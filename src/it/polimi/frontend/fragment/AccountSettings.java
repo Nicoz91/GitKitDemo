294d3d67dc2524dac2b584449062e4aa5913c157
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-01-30 08:52:07 +0100

    aggiustamenti Account Settings. TODO: migliorare aspetto grafico

--- src/it/polimi/frontend/fragment/AccountSettings.java
+++ src/it/polimi/frontend/fragment/AccountSettings.java
@@ -20,6 +20,10 @@ import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.api.client.util.DateTime;
 
 public class AccountSettings extends Fragment implements OnClickListener, DatePickerDialog.OnDateSetListener{
 
@@ -27,18 +31,16 @@ public class AccountSettings extends Fragment implements OnClickListener, DatePi
 
 	private Calendar data;
 
-	private EditText nameET;
-	private EditText surnameET;
-	private EditText pwAccountET;
-	private EditText gmailAccountET;
-	private EditText fbAccountET;
-	private EditText bDayET;
+	private EditText nameET,surnameET,accountET[],bDayET;
+	private TextView nameTV,surnameTV,accountTV[],bDayTV;
 	private RadioButton maleRB;
 	private RadioButton femaleRB;
 	private boolean male;
 	private Menu menu;
 	private boolean editMode;
 	private User user;
+	private final static int PW=0,GMAIL=1,FB=2;
+	private boolean accountType[]={false,false,false};
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,32 +48,76 @@ public class AccountSettings extends Fragment implements OnClickListener, DatePi
 				container, false);
 		setHasOptionsMenu(true);
 		user = QueryManager.getInstance().getCurrentUser();
+		initializeView(rootView);
+		return rootView;
+	}
+
+	private void initializeView(View rootView){
+		accountET = new EditText[3];
+		accountTV = new TextView[3];
+		//Setup EditText
 		nameET = (EditText) rootView.findViewById(R.id.name);
 		nameET.setText(user.getName());
 		surnameET = (EditText) rootView.findViewById(R.id.surname);
 		surnameET.setText(user.getSurname());
-		pwAccountET = (EditText) rootView.findViewById(R.id.pwAccount);
-		pwAccountET.setText(user.getPwAccount());
-		gmailAccountET = (EditText) rootView.findViewById(R.id.gmailAccount);
-		gmailAccountET.setText(user.getGmailAccount());
-		fbAccountET = (EditText) rootView.findViewById(R.id.fbAccount);
-		fbAccountET.setText(user.getFbAccount());
+		accountET[PW] = (EditText) rootView.findViewById(R.id.pwAccount);
+		accountET[PW].setText(user.getPwAccount());
+		accountET[GMAIL] = (EditText) rootView.findViewById(R.id.gmailAccount);
+		accountET[GMAIL].setText(user.getGmailAccount());
+		accountET[FB] = (EditText) rootView.findViewById(R.id.fbAccount);
+		accountET[FB].setText(user.getFbAccount());
+
+		//Setup TextView
+		nameTV = (TextView) rootView.findViewById(R.id.nameTV);
+		nameTV.setText(user.getName());
+		surnameTV = (TextView) rootView.findViewById(R.id.surnameTV);
+		surnameTV.setText(user.getSurname());
+		accountTV[PW] = (TextView) rootView.findViewById(R.id.pwAccountTV);
+		accountTV[PW].setText(user.getPwAccount());
+		accountTV[GMAIL] = (TextView) rootView.findViewById(R.id.gmailAccountTV);
+		accountTV[GMAIL].setText(user.getGmailAccount());
+		accountTV[FB] = (TextView) rootView.findViewById(R.id.fbAccountTV);
+		accountTV[FB].setText(user.getFbAccount());
+
+		//Setup bDay
 		bDayET = (EditText) rootView.findViewById(R.id.bDay);
+		bDayTV = (TextView) rootView.findViewById(R.id.bDayTV);
 		if (user.getBday()!=null){
 			Calendar c = Calendar.getInstance();
 			c.setTimeInMillis(user.getBday().getValue());
 			this.onDateSet(null, c.get(Calendar.YEAR),c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
 		}
 		bDayET.setOnClickListener(this);
+
+		//Setup gender
 		maleRB = (RadioButton) rootView.findViewById(R.id.radio_male);
 		femaleRB = (RadioButton) rootView.findViewById(R.id.radio_female);
+		maleRB.setOnClickListener(this);
+		femaleRB.setOnClickListener(this);
 		if (user.getGender())
 			maleRB.performClick();
 		else 
 			femaleRB.performClick();
+		
+		//Setup degli account visibili
+		if (user.getPwAccount()!=null)
+			accountType[PW]=true;
+		else {
+			((TextView)rootView.findViewById(R.id.pwRow)).setVisibility(View.GONE);
+		}
+		if (user.getGmailAccount()!=null)
+			accountType[GMAIL]=true;
+		else {
+			((TextView)rootView.findViewById(R.id.gmailRow)).setVisibility(View.GONE);
+		}
+		if (user.getFbAccount()!=null)
+			accountType[FB]=true;
+		else {
+			((TextView)rootView.findViewById(R.id.fbRow)).setVisibility(View.GONE);
+		}
+		//Setup NonEditable Mode
 		editMode=false;
 		editable(editMode);
-		return rootView;
 	}
 
 	@Override
@@ -100,33 +146,42 @@ public class AccountSettings extends Fragment implements OnClickListener, DatePi
 			menu.findItem(R.id.saveAccount).setVisible(false);
 			editMode=false;
 			editable(editMode);
-
+			if (hasChanged())
+				updateUser();
+			else
+				Toast.makeText(getActivity().getApplicationContext(), "Nulla è cambiato.",
+						Toast.LENGTH_SHORT).show();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
 
+	private void updateUser(){
+		user.setName(nameET.getEditableText().toString());
+		user.setSurname(surnameET.getEditableText().toString());
+		user.setPwAccount(accountET[PW].getEditableText().toString());
+		user.setGmailAccount(accountET[GMAIL].getEditableText().toString());
+		user.setFbAccount(accountET[FB].getEditableText().toString());
+		user.setGender(male);
+		user.setBday(new DateTime(data.getTimeInMillis()));
+		QueryManager.getInstance().updateUser();
+		Toast.makeText(getActivity().getApplicationContext(), "Dati utente aggiornati correttamente.",
+				Toast.LENGTH_SHORT).show();
+	}
+
 	private boolean hasChanged(){
-		boolean changed=false, dataEraPresente=false;
-		if (user.getBday()!=null){
-			Calendar c = Calendar.getInstance();
-			c.setTimeInMillis(user.getBday().getValue());
-			dataEraPresente=true;
-		}else{
-			dataEraPresente=false;
-		}
-		if (nameET.getEditableText().toString().equals(user.getName())
-				&& surnameET.getEditableText().toString().equals(user.getSurname())
-				&& pwAccountET.getEditableText().toString().equals(user.getPwAccount())
-				&& gmailAccountET.getEditableText().toString().equals(user.getGmailAccount())
-				&& fbAccountET.getEditableText().toString().equals(user.getFbAccount())
-				&& surnameET.getEditableText().toString().equals(user.getName())
+		boolean changed=false;
+		if (nameET.getEditableText().toString().equals(nameTV.getText().toString())
+				&& surnameET.getEditableText().toString().equals(surnameTV.getText().toString())
+				&& accountET[PW].getEditableText().toString().equals(accountTV[PW].getText().toString())
+				&& accountET[GMAIL].getEditableText().toString().equals(accountTV[GMAIL].getText().toString())
+				&& accountET[FB].getEditableText().toString().equals(accountTV[FB].getText().toString())
 				&& male==user.getGender()
-				&& (!dataEraPresente && data==null) 
-				//TODO
-				)
+				&& bDayET.getEditableText().toString().equals(bDayTV.getText().toString()))
 			changed=false;
+		else
+			changed=true;
 		return changed;
 	}
 
@@ -154,6 +209,9 @@ public class AccountSettings extends Fragment implements OnClickListener, DatePi
 		bDayET.setText(data.get(Calendar.DAY_OF_MONTH)+" "+
 				data.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ITALIAN)
 				+" "+data.get(Calendar.YEAR));
+		bDayTV.setText(data.get(Calendar.DAY_OF_MONTH)+" "+
+				data.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ITALIAN)
+				+" "+data.get(Calendar.YEAR));
 	}
 
 	@Override
@@ -174,23 +232,33 @@ public class AccountSettings extends Fragment implements OnClickListener, DatePi
 	}
 
 	private void editable(boolean editable){
-		nameET.setFocusable(editable);
-		nameET.setFocusableInTouchMode(editable);
-		nameET.setClickable(editable);
-		surnameET.setFocusable(editable);
-		surnameET.setFocusableInTouchMode(editable);
-		surnameET.setClickable(editable);
-		pwAccountET.setFocusable(editable);
-		pwAccountET.setFocusableInTouchMode(editable);
-		pwAccountET.setClickable(editable);
-		gmailAccountET.setFocusable(editable);
-		gmailAccountET.setFocusableInTouchMode(editable);
-		gmailAccountET.setClickable(editable);
-		fbAccountET.setFocusable(editable);
-		fbAccountET.setFocusableInTouchMode(editable);
-		fbAccountET.setClickable(editable);
-
-		bDayET.setClickable(editable);
+		if (editable){
+			nameTV.setVisibility(View.GONE);
+			nameET.setVisibility(View.VISIBLE);
+			surnameTV.setVisibility(View.GONE);
+			surnameET.setVisibility(View.VISIBLE);
+			bDayTV.setVisibility(View.GONE);
+			bDayET.setVisibility(View.VISIBLE);
+			for(int i=0; i<accountType.length;i++){
+				if (accountType[i]){//solo se è tra i tipi di account visibili
+					accountTV[i].setVisibility(View.GONE);
+					accountET[i].setVisibility(View.VISIBLE);
+				}
+			}
+		} else {
+			nameTV.setVisibility(View.VISIBLE);
+			nameET.setVisibility(View.GONE);
+			surnameTV.setVisibility(View.VISIBLE);
+			surnameET.setVisibility(View.GONE);
+			bDayTV.setVisibility(View.VISIBLE);
+			bDayET.setVisibility(View.GONE);
+			for(int i=0; i<accountType.length;i++){
+				if (accountType[i]){//solo se è tra i tipi di account visibili
+					accountTV[i].setVisibility(View.VISIBLE);
+					accountET[i].setVisibility(View.GONE);
+				}
+			}
+		}
 		maleRB.setClickable(editable);
 		femaleRB.setClickable(editable);
 	}
