5b954a09d59f78f57880fd308b9cdfccf25157d3
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-01-30 13:58:19 +0100

    qualche modifica a request detail

--- src/it/polimi/frontend/fragment/RequestDetail.java
+++ src/it/polimi/frontend/fragment/RequestDetail.java
@@ -11,8 +11,10 @@ import it.polimi.frontend.util.QueryManager;
 import it.polimi.frontend.util.UserAdapter;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -31,6 +33,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 //import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
 
 public class RequestDetail extends Fragment implements OnClickListener, OnItemClickListener{
 
@@ -78,17 +81,30 @@ public class RequestDetail extends Fragment implements OnClickListener, OnItemCl
 			ll.setOnClickListener(this);
 
 		if (request!=null){
+			//Photo
 			if (request.getOwner()!= null && request.getOwner().getPhotoURL()!=null)
 				new ProfileImageTask().execute(request.getOwner().getPhotoURL());
+			//Name and surname
 			((TextView)rootView.findViewById(R.id.ownerLabel))
 			.setText(request.getOwner().getName()+" "+request.getOwner().getSurname());
+			//Age and sex
+			String subText="";
+			if (request.getOwner().getGender())
+				subText+="M, ";
+			else
+				subText+="F, ";
 			if (request.getOwner().getBday()!=null){
-				((TextView)rootView.findViewById(R.id.ageSexLabel)).setText(request.getOwner().getBday().toString());
-			} else {
-				((TextView)rootView.findViewById(R.id.ageSexLabel)).setText("Età non specificata.");
-			}
+				Calendar dob = Calendar.getInstance();	
+				dob.setTimeInMillis(request.getOwner().getBday().getValue());
+				Calendar now = Calendar.getInstance();
+				subText+=getAge(dob,now);
+			} else 
+				subText+="Età non specificata";
+			((TextView)rootView.findViewById(R.id.ageSexLabel)).setText(subText);
+			//Titolo richiesta
 			((TextView)rootView.findViewById(R.id.titleLabel))
-			.setText(request.getTitle());
+			.setText("Titolo: "+request.getTitle());
+			//Descrizione
 			((TextView)rootView.findViewById(R.id.descriptionLabel))
 			.setText(request.getDescription());
 			userPartecipant = (ListView) rootView.findViewById(R.id.partecipantsList);
@@ -108,24 +124,6 @@ public class RequestDetail extends Fragment implements OnClickListener, OnItemCl
 		//TODO
 	}
 
-	private class ProfileImageTask extends AsyncTask<String, Void, Bitmap>{
-		@Override
-		protected Bitmap doInBackground(String... arg) {
-			try {
-				byte[] result = HttpUtils.get(arg[0]);
-				return BitmapFactory.decodeByteArray(result, 0, result.length);
-			} catch (IOException e) {
-				return null;
-			}
-		}
-
-		@Override
-		protected void onPostExecute(Bitmap bitmap) {
-			if (bitmap != null) {
-				profileImg.setImageBitmap(bitmap);
-			}
-		}
-	}
 
 	/**
 	 * Interfaccia che deve implementare il master (fragment o activity) per mostrare i 
@@ -145,12 +143,12 @@ public class RequestDetail extends Fragment implements OnClickListener, OnItemCl
 		case R.id.joinReq:
 			if(request.getPartecipants()==null || !request.getPartecipants().contains((QueryManager.getInstance().getCurrentUser().getId())) ){
 				QueryManager.getInstance().joinRequest(request);
-				//QueryManager.getInstance().insertFeedback();
+				//new JoinTask().execute(true);
 				join.setText("Cancella");
 			}
 			else{
 				QueryManager.getInstance().removeJoinRequest(request);
-				//QueryManager.getInstance().insertFeedback();
+				//new JoinTask().execute(false);
 				join.setText("Partecipa");
 			}
 			break;
@@ -181,8 +179,93 @@ public class RequestDetail extends Fragment implements OnClickListener, OnItemCl
 		}
 	}
 
+	private int getAge(Calendar dob, Calendar now){
+		int age=0;
+		age = now.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+	    if ((dob.get(Calendar.MONTH) > now.get(Calendar.MONTH))
+	        || (dob.get(Calendar.MONTH) == now.get(Calendar.MONTH) && dob.get(Calendar.DAY_OF_MONTH) > now
+	            .get(Calendar.DAY_OF_MONTH))) {
+	      age--;
+	    }
+		return age;
+	}
+	
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
 		listener.onUserClicked((User) userPartecipant.getAdapter().getItem(position),request.getId());
+	}
+	
+	private class JoinTask extends AsyncTask<Boolean, Void, Boolean>{
+		
+		@Override
+		protected void onPreExecute() {
+			showDialog();
+			super.onPreExecute();
+		}
+
+		@Override
+		protected Boolean doInBackground(Boolean... arg) {
+			System.out.println("prima dell'invocazione su queryManager");
+			if (arg[0].booleanValue())
+				QueryManager.getInstance().joinRequest(request);
+			else
+				QueryManager.getInstance().removeJoinRequest(request);
+			System.out.println("Prima della return");
+			return arg[0];
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean add) {
+			hideDialog();
+			System.out.println("Dovrei aver nascosto il dialog");
+			if (add.booleanValue())
+				Toast.makeText(getActivity().getApplicationContext(), "Inserita correttamente la partecipazione.",
+						Toast.LENGTH_SHORT).show();
+			else 
+				Toast.makeText(getActivity().getApplicationContext(), "Rimossa correttamente la partecipazione.",
+						Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	private class ProfileImageTask extends AsyncTask<String, Void, Bitmap>{
+		@Override
+		protected Bitmap doInBackground(String... arg) {
+			try {
+				byte[] result = HttpUtils.get(arg[0]);
+				return BitmapFactory.decodeByteArray(result, 0, result.length);
+			} catch (IOException e) {
+				return null;
+			}
+		}
+		
+		@Override
+		protected void onPostExecute(Bitmap bitmap) {
+			if (bitmap != null && profileImg!=null) {
+				profileImg.setImageBitmap(bitmap);
+			}
+		}
+	}
+	
+	/** 
+	 * Metodi per mostrare o meno il progressDialog
+	 * */
+	private ProgressDialog mProgressDialog;
+	protected void showDialog() {
+		if (mProgressDialog == null) {
+			setProgressDialog();
+		}
+		mProgressDialog.show();
+	}
+
+	protected void hideDialog() {
+		if (mProgressDialog != null && mProgressDialog.isShowing()) {
+			mProgressDialog.dismiss();
+		}
+	}
+
+	private void setProgressDialog() {
+		mProgressDialog = new ProgressDialog(getActivity());
+		mProgressDialog.setTitle("Attendi...");
+		mProgressDialog.setMessage("Sto scaricando...");
 	}
 }
