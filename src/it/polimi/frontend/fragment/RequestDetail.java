67fc62adceec4e00dbff785e06ad92918cbc8574
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-02-01 15:23:57 +0100

    pull

--- src/it/polimi/frontend/fragment/RequestDetail.java
+++ src/it/polimi/frontend/fragment/RequestDetail.java
@@ -1,6 +1,5 @@
 package it.polimi.frontend.fragment;
 
-import it.polimi.appengine.entity.manager.model.Feedback;
 import it.polimi.appengine.entity.manager.model.Request;
 import it.polimi.appengine.entity.manager.model.User;
 import it.polimi.frontend.activity.HttpUtils;
@@ -66,12 +65,14 @@ public class RequestDetail extends Fragment implements OnClickListener, OnItemCl
 				container, false);
 		profileImg = (ImageView) rootView.findViewById(R.id.profileImg);
 		join = (Button) rootView.findViewById(R.id.joinReq);
+		sendFb = (Button) rootView.findViewById(R.id.showPartecipant);
 		if(request.getPartecipants()==null || !request.getPartecipants().contains((QueryManager.getInstance().getCurrentUser().getId())))
 			join.setText("Partecipa");
 		else
 			join.setText("Cancella");
 
 		join.setOnClickListener(this);
+		sendFb.setOnClickListener(this);
 		LinearLayout ll = (LinearLayout)rootView.findViewById(R.id.userSection);
 		if (mode==OWNER_REQUEST){
 			ll.setVisibility(View.GONE);
