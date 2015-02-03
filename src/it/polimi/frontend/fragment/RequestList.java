774a9cd1f2378bbea0a3f49ffcf78f0199bd40fe
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-02-03 17:40:14 +0100

    Gestione pull-to-refresh: RequestList non è più figlia diretta di un ListFragment ma di un generico Fragment, così da potergli assegnare il nuovo layout con la lista inclusa in uno SwipeRefreshLayout. Aggiustate le chiamate a metodi specifici di ListFragment e aggiunti i riferimenti alla lista ora nel layout + la gestione del refresh dei dati.

--- src/it/polimi/frontend/fragment/RequestList.java
+++ src/it/polimi/frontend/fragment/RequestList.java
@@ -2,19 +2,27 @@ package it.polimi.frontend.fragment;
 
 import it.polimi.appengine.entity.manager.model.Request;
 import it.polimi.frontend.activity.MyApplication;
+import it.polimi.frontend.activity.R;
 import it.polimi.frontend.util.ParentFragmentUtil;
+import it.polimi.frontend.util.QueryManager;
 import it.polimi.frontend.util.RequestAdapter;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-public class RequestList extends ListFragment /*implements OnRequestLoadedListener*/{
+public class RequestList extends Fragment implements OnItemClickListener,SwipeRefreshLayout.OnRefreshListener/*implements OnRequestLoadedListener*/{
 
 	OnRequestSelectedListener mListener;
 	public final static String ID="RequestListFragmentID";
@@ -23,10 +31,12 @@ public class RequestList extends ListFragment /*implements OnRequestLoadedListen
 	public final static int JOINED_REQUEST=2;
 	private int listMode=0;
 	private List<Request> requests;
+	private ListView requestList;
+	private SwipeRefreshLayout swipeRefresh;
 
 	public RequestList(){
 	}
-	
+
 	public RequestList(List<Request> reqs, int mode){
 		requests=reqs;
 		this.listMode=mode;
@@ -79,22 +89,44 @@ public class RequestList extends ListFragment /*implements OnRequestLoadedListen
 			System.out.println("Le richieste sono nulle? Le sto ancora caricando?");
 			//RequestLoader.getInstance().loadRequest();
 		}*/
+		View rootView = inflater.inflate(R.layout.fragment_request_list,
+				container, false);
+		swipeRefresh = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeRefresh);
+		swipeRefresh.setOnRefreshListener(this);
+		//TODO
+		requestList = (ListView) rootView.findViewById(R.id.requestList);
+		requestList.setOnItemClickListener(this);
+		//Per attivare swipeRefresh solo se in cima alla lista 
+		requestList.setOnScrollListener(new AbsListView.OnScrollListener() {  
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+				int topRowVerticalPosition = 
+						(requestList == null || requestList.getChildCount() == 0) ? 
+								0 : requestList.getChildAt(0).getTop();
+				swipeRefresh.setEnabled(firstVisibleItem == 0 && topRowVerticalPosition >= 0);
+			}
+		});
 		if(requests!=null && requests.size()>0 ){
 			setRequestAdapter(requests);	
 		}
 		else{
-//			System.out.println("Le richieste sono nulle? Le sto ancora caricando?");
+			//			System.out.println("Le richieste sono nulle? Le sto ancora caricando?");
 			setRequestAdapter(new ArrayList<Request>());
 			//RequestLoader.getInstance().loadRequest();
 		}
 
 		mListener = ParentFragmentUtil.getParent(this, OnRequestSelectedListener.class);
-		return super.onCreateView(inflater, container, savedInstanceState);
+		return rootView;
 	}
 
 	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		mListener.onRequestSelected(position, (Request) getListAdapter().getItem(position));
+	public void onItemClick(AdapterView<?> list, View v, int position, long id) {
+		mListener.onRequestSelected(position, (Request) requestList.getAdapter().getItem(position));
 	}
 
 	/**
@@ -107,16 +139,18 @@ public class RequestList extends ListFragment /*implements OnRequestLoadedListen
 
 	//@Override
 	public void onRequestLoaded(List<Request> requests) {
-//		System.out.println("Ho ricevuto requests:");
+		//		System.out.println("Ho ricevuto requests:");
 		if(requests==null){
-//			System.out.println("Requests è nullo");
+			//			System.out.println("Requests è nullo");
 		}
 		else
 			setRequestAdapter(requests);		
 	}
 
 	public void setRequestAdapter(List<Request> requests){
-//		System.out.println("Setto le richieste");
+		//		System.out.println("Setto le richieste");
+		if (swipeRefresh!=null)
+			swipeRefresh.setRefreshing(false);
 		List<Request> reqs;
 		if(requests == null)
 			reqs = new ArrayList<Request>();
@@ -124,17 +158,25 @@ public class RequestList extends ListFragment /*implements OnRequestLoadedListen
 			reqs= requests;
 		Context c = getActivity();
 		if(c==null){ 
-//			System.out.println("Il context è null ma noi bariamo");
+			//			System.out.println("Il context è null ma noi bariamo");
 			c = MyApplication.getContext();
 		}
 		RequestAdapter adapter = new RequestAdapter(c,0,reqs,this.listMode);
-		setListAdapter(adapter);
+		requestList.setAdapter(adapter);
 	}
 
 	public void setActivateOnItemClick(boolean activateOnItemClick) {
-		getListView().setChoiceMode(
-				activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
-						: ListView.CHOICE_MODE_NONE);
+		if (requestList!=null)
+			requestList.setChoiceMode(
+					activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
+							: ListView.CHOICE_MODE_NONE);
 	}
+
+	@Override
+	public void onRefresh() {
+		// TODO Auto-generated method stub
+		QueryManager.getInstance().loadRequest();
+	}
+
 
 }
