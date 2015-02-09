4b6fd6b97f12821d7a71a869e4e12d466f8db55c
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-02-09 18:09:52 +0100

    Aggiunte alcune icone anche al menu di insertRequest (ma alcune inspiegabilmente non le trova nonostante debbano esserci). Sostituito l'edittext del tag con uno spinner, e risolto un problema che sorgeva premendo avanti quando tentava di parsare la stringa "" come intero per settare max partecipant.

--- src/it/polimi/frontend/fragment/InsertRequest.java
+++ src/it/polimi/frontend/fragment/InsertRequest.java
@@ -1,14 +1,13 @@
 package it.polimi.frontend.fragment;
 
-import java.util.Calendar;
-
-import com.google.api.client.util.DateTime;
-
 import it.polimi.appengine.entity.manager.model.Request;
 import it.polimi.frontend.activity.MyApplication;
 import it.polimi.frontend.activity.R;
 import it.polimi.frontend.util.TextValidator;
 import it.polimi.frontend.util.DateSlider.labeler.TimeLabeler;
+
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,11 +15,15 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.api.client.util.DateTime;
 
 
 public class InsertRequest extends Fragment{
@@ -30,14 +33,15 @@ public class InsertRequest extends Fragment{
 	//private GoogleMap map;
 	private EditText title;
 	private EditText description;
-	private EditText tag;
+	//private EditText tag;
+	private Spinner tag;
 	private EditText start;
 	private EditText end;
 	private EditText max;
 	private Calendar startDate;
 	private Calendar endDate;
 	private boolean valid;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -56,7 +60,7 @@ public class InsertRequest extends Fragment{
 				}
 			}
 		});
-		
+
 		description = (EditText)rootView.findViewById(R.id.insert_description_edit);
 		description.addTextChangedListener(new TextValidator(description){
 			@Override
@@ -68,7 +72,7 @@ public class InsertRequest extends Fragment{
 					textView.setError("Aggiungi una descrizione.");
 				}
 			}
-		});
+		});/*Vecchio codice con EditText
 		tag = (EditText) rootView.findViewById(R.id.insert_tag_edit);
 		tag.addTextChangedListener(new TextValidator(tag){
 			@Override
@@ -80,7 +84,16 @@ public class InsertRequest extends Fragment{
 					textView.setError("Inserisci almeno un tag per cercare la richiesta!");
 				}
 			}
-		});
+		});*/
+		tag = (Spinner) rootView.findViewById(R.id.insert_tag_spin);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+				R.array.tag_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		tag.setAdapter(adapter);
+
 		Calendar now = Calendar.getInstance();
 		start = (EditText) rootView.findViewById(R.id.insert_start_edit);
 		start.setText(String.format("%n%te. %tB %tY%n%tH:%02d",
@@ -114,9 +127,9 @@ public class InsertRequest extends Fragment{
 				TimeLabeler.MINUTEINTERVAL*TimeLabeler.MINUTEINTERVAL;
 		start.setText(String.format("%n%te. %tB %tY%n%tH:%02d",
 				selectedDate, selectedDate, selectedDate, selectedDate, minute));
-		
+
 	}
-	
+
 	public void onEndDateSelected(Calendar selectedDate){
 		endDate = selectedDate;
 		int minute = selectedDate.get(Calendar.MINUTE) /
@@ -140,7 +153,7 @@ public class InsertRequest extends Fragment{
 		int id = item.getItemId();
 		switch (id) {
 		case R.id.insert_next:
-//			System.out.println("Vado avanti");
+			//			System.out.println("Vado avanti");
 			return true;
 		case R.id.insert_cancel:
 
@@ -152,7 +165,8 @@ public class InsertRequest extends Fragment{
 
 	public boolean assignAttribute(Request req){
 
-		if(title.getText().equals("") || description.getText().equals("") || tag.getText().equals(""))
+		//if(title.getText().equals("") || description.getText().equals("") || tag.getText().equals(""))
+		if(title.getText().equals("") || description.getText().equals("") || tag.getSelectedItem().toString().equals(""))
 			return false;
 		if(startDate.getTimeInMillis()>endDate.getTimeInMillis()){
 			Toast.makeText(MyApplication.getContext(),"La data di fine non può essere precedente a quella di inizio",Toast.LENGTH_SHORT).show();
@@ -160,10 +174,14 @@ public class InsertRequest extends Fragment{
 		}
 		req.setTitle(title.getText().toString());
 		req.setDescription(description.getText().toString());
-		req.setType(tag.getText().toString());
+		//req.setType(tag.getText().toString());
+		req.setType(tag.getSelectedItem().toString());
 		req.setStart(new DateTime(startDate.getTimeInMillis()));
 		req.setEnd(new DateTime(endDate.getTimeInMillis()));
-		req.setMaxPartecipants(Integer.parseInt(max.getText().toString()));	
+		if (max.getText().toString()==null || max.getText().toString().equals(""))
+			req.setMaxPartecipants(0);
+		else
+			req.setMaxPartecipants(Integer.parseInt(max.getText().toString()));	
 
 		return valid;
 	}
