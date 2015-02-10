72bc39281862e96f70521489a464ae19626a9bab
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-02-10 10:33:34 +0100

    Aggiunta libreria per download immagini, e tentativo di usarla nel requestAdapter. TODO: verificare che con essa funzioni come ci si aspetta

--- src/it/polimi/frontend/util/RequestAdapter.java
+++ src/it/polimi/frontend/util/RequestAdapter.java
@@ -1,17 +1,22 @@
 package it.polimi.frontend.util;
 
+import it.polimi.appengine.entity.manager.model.Request;
+import it.polimi.frontend.activity.HttpUtils;
+import it.polimi.frontend.activity.R;
+
 import java.io.IOException;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
-import it.polimi.appengine.entity.manager.model.Request;
-import it.polimi.frontend.activity.HttpUtils;
-import it.polimi.frontend.activity.R;
+import com.squareup.picasso.Picasso;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +28,7 @@ public class RequestAdapter extends ArrayAdapter<Request>{
 
 	private Context context;
 	private List<Request> reqs;
-	private View rowView;
+	//private View rowView;
 	public final static int ALL_REQUEST=0;
 	public final static int OWNER_REQUEST=1;
 	public final static int JOINED_REQUEST=2;
@@ -39,14 +44,15 @@ public class RequestAdapter extends ArrayAdapter<Request>{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView;
 		if (convertView==null){
 			rowView = inflater.inflate(R.layout.row_layout_request, parent, false);
 		} else {
 			rowView = convertView;
 		}
-//		System.out.println("Position: "+position);
+		//		System.out.println("Position: "+position);
 		try{
-			
+
 			TextView textView = (TextView) rowView.findViewById(R.id.txtTitle);
 			TextView textView2 = (TextView) rowView.findViewById(R.id.subString);
 			switch (listMode) {
@@ -79,15 +85,16 @@ public class RequestAdapter extends ArrayAdapter<Request>{
 				textView.setText(reqs.get(position).getOwner().getName()+" "+
 						reqs.get(position).getOwner().getSurname()+" ");
 				textView2.setText(reqs.get(position).getTitle());
-				
+				ImageView iV = (ImageView) rowView.findViewById(R.id.imgIcon);
 				//TODO task profile image
 				String photoUrl=reqs.get(position).getOwner().getPhotoURL();
 				if (photoUrl!=null){
-					new ProfileImageTask().execute(photoUrl);
+					Picasso.with(context).load(photoUrl).into(iV);
+					//new ProfileImageTask().execute(photoUrl);
 				}
 				break;
 			}
-			
+
 
 		}catch(Exception e){
 			System.out.println("Trovata eccezione: Probabilmente uno dei campi null");
@@ -108,6 +115,7 @@ public class RequestAdapter extends ArrayAdapter<Request>{
 
 		@Override
 		protected void onPostExecute(Bitmap bitmap) {
+			View rowView=null; //TODO tolta temporaneamente var globale statica
 			if (bitmap != null) {
 				ImageView pictureView = (ImageView) rowView.findViewById(R.id.imgIcon);
 				pictureView.setImageBitmap(bitmap);
@@ -115,4 +123,35 @@ public class RequestAdapter extends ArrayAdapter<Request>{
 		}
 	}
 
+	private static class ViewHolder {
+		ImageView imageView;
+		Bitmap bitmap;
+		String imageURL;
+	}
+
+
+	private class DownloadAsyncTask extends AsyncTask<ViewHolder, Void, ViewHolder> {
+		@Override
+		protected ViewHolder doInBackground(ViewHolder... params) {
+			//load image directly
+			ViewHolder viewHolder = params[0];
+			try {
+				URL imageURL = new URL(viewHolder.imageURL);
+				viewHolder.bitmap = BitmapFactory.decodeStream(imageURL.openStream());
+			} catch (IOException e) {
+				Log.e("error", "Downloading Image Failed");
+				viewHolder.bitmap = null;
+			}
+
+			return viewHolder;
+		}
+
+		@Override
+		protected void onPostExecute(ViewHolder result) {
+			if (result.bitmap != null)
+				result.imageView.setImageBitmap(result.bitmap);
+		}
+	}
 }
+
+
