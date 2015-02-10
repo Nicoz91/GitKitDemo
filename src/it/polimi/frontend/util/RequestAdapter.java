f631dad0a6fb12610b3903e89af53ba26ab9bc7f
Nikoz91 <niccolo.zamperetti@gmail.com>
2015-02-10 11:13:46 +0100

    Sistemato e pulito RequestAdapter

--- src/it/polimi/frontend/util/RequestAdapter.java
+++ src/it/polimi/frontend/util/RequestAdapter.java
@@ -1,11 +1,8 @@
 package it.polimi.frontend.util;
 
 import it.polimi.appengine.entity.manager.model.Request;
-import it.polimi.frontend.activity.HttpUtils;
 import it.polimi.frontend.activity.R;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -13,10 +10,6 @@ import java.util.Locale;
 import com.squareup.picasso.Picasso;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,129 +21,105 @@ public class RequestAdapter extends ArrayAdapter<Request>{
 
 	private Context context;
 	private List<Request> reqs;
-	//private View rowView;
 	public final static int ALL_REQUEST=0;
 	public final static int OWNER_REQUEST=1;
 	public final static int JOINED_REQUEST=2;
 	private int listMode=0;
+	private String NON_SPECIFICATO;
 
 	public RequestAdapter(Context context, int resource, List<Request> objects, int listMode) {
 		super(context, R.layout.row_layout_request, objects);
 		this.context=context;
 		this.reqs=objects;
 		this.listMode=listMode;
+		NON_SPECIFICATO=context.getString(R.string.not_specified);
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View rowView;
-		if (convertView==null){
+		if (convertView==null)
 			rowView = inflater.inflate(R.layout.row_layout_request, parent, false);
-		} else {
+		else 
 			rowView = convertView;
-		}
-		//		System.out.println("Position: "+position);
-		try{
-
-			TextView textView = (TextView) rowView.findViewById(R.id.txtTitle);
-			TextView textView2 = (TextView) rowView.findViewById(R.id.subString);
-			switch (listMode) {
-			case OWNER_REQUEST:
+		TextView textView = (TextView) rowView.findViewById(R.id.txtTitle);
+		TextView textView2 = (TextView) rowView.findViewById(R.id.subString);
+		switch (listMode) {
+		case OWNER_REQUEST:
+			//Upper: Titolo
+			if(reqs.get(position).getTitle()!=null)
 				textView.setText(reqs.get(position).getTitle());
-				((ImageView) rowView.findViewById(R.id.imgIcon)).setVisibility(View.GONE);
-				if (reqs.get(position).getStart()!=null && reqs.get(position).getEnd()!=null){
-					//Start date
-					Calendar start = Calendar.getInstance();
-					start.setTimeInMillis(reqs.get(position).getStart().getValue());
-					String dataInizio = start.get(Calendar.DAY_OF_MONTH)+" "+
-							start.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ITALIAN)
-							+" "+start.get(Calendar.YEAR);
-					//Duration
-					long durationMs = reqs.get(position).getStart().getValue()-reqs.get(position).getEnd().getValue();
-					long duration = durationMs / 1000;
-					long h = duration / 3600;
-					long m = (duration - h * 3600) / 60;
-					long s = duration - (h * 3600 + m * 60);
-					String durationValue;
-					if (h == 0)
-						durationValue = m+"m:"+s+"s";
-					else 
-						durationValue = h+"h:"+m+"m:"+s+"s";
-					textView2.setText("Data: "+dataInizio+", Durata: "+durationValue);
-				} else 
-					textView2.setText("Tempi non specificati.");
-				break;
-			default: //ALL_REQUEST e JOINED REQUEST al momento
-				textView.setText(reqs.get(position).getOwner().getName()+" "+
-						reqs.get(position).getOwner().getSurname()+" ");
-				textView2.setText(reqs.get(position).getTitle());
-				ImageView iV = (ImageView) rowView.findViewById(R.id.imgIcon);
-				//TODO task profile image
-				String photoUrl=reqs.get(position).getOwner().getPhotoURL();
-				if (photoUrl!=null){
+			else
+				textView.setText(NON_SPECIFICATO);
+			//ProfileImage nascosta
+			((ImageView) rowView.findViewById(R.id.imgIcon)).setVisibility(View.GONE);
+
+			//Lower: Data, durata
+			String lower=context.getString(R.string.startDateLabel);
+			//Data
+			if (reqs.get(position).getStart()!=null){
+				Calendar start = Calendar.getInstance();
+				start.setTimeInMillis(reqs.get(position).getStart().getValue());
+				String dataInizio = start.get(Calendar.DAY_OF_MONTH)+" "+
+						start.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ITALIAN)
+						+" "+start.get(Calendar.YEAR);
+				lower+=dataInizio+", "+context.getString(R.string.durationLabel);
+			} else
+				lower+=NON_SPECIFICATO+", "+context.getString(R.string.durationLabel);
+			//Durata
+			if(reqs.get(position).getEnd()!=null){
+				long durationMs = reqs.get(position).getEnd().getValue()-reqs.get(position).getStart().getValue();
+				long duration = durationMs / 1000;
+				long h = duration / 3600;
+				long m = (duration - h * 3600) / 60;
+				long s = duration - (h * 3600 + m * 60);
+				String durationValue;
+				if (h == 0)
+					durationValue = m+"m:"+s+"s";
+				else 
+					durationValue = h+"h:"+m+"m:"+s+"s";
+				lower+=durationValue;
+			} else
+				lower+=NON_SPECIFICATO;
+			textView2.setText(lower);
+			break;
+		default: //ALL_REQUEST e JOINED REQUEST al momento
+			if (reqs.get(position).getOwner()!=null) {
+				//Nome Cognome
+				String upper="";
+				if (reqs.get(position).getOwner().getName()!=null) 
+					upper+=reqs.get(position).getOwner().getName()+" ";
+				else
+					upper+=NON_SPECIFICATO+" ";
+				if (reqs.get(position).getOwner().getSurname()!=null) 
+					upper+=reqs.get(position).getOwner().getSurname();
+				else
+					upper+=NON_SPECIFICATO;
+				textView.setText(upper);
+				//Titolo
+				if(reqs.get(position).getTitle()!=null)
+					textView2.setText(reqs.get(position).getTitle());
+				else
+					textView2.setText(NON_SPECIFICATO);
+				//Photo
+				String photoUrl = reqs.get(position).getOwner().getPhotoURL();
+				if (photoUrl != null){
+					ImageView iV = (ImageView) rowView.findViewById(R.id.imgIcon);
 					Picasso.with(context).load(photoUrl).into(iV);
-					//new ProfileImageTask().execute(photoUrl);
 				}
-				break;
+			} else {
+				//Upper
+				textView.setText(NON_SPECIFICATO);
+				//Lower
+				if(reqs.get(position).getTitle()!=null)
+					textView2.setText(reqs.get(position).getTitle());
+				else
+					textView2.setText(NON_SPECIFICATO);
 			}
-
-
-		}catch(Exception e){
-			System.out.println("Trovata eccezione: Probabilmente uno dei campi null");
-			e.printStackTrace();
+			break;
 		}
 		return rowView;
-	}
-	private class ProfileImageTask extends AsyncTask<String, Void, Bitmap>{
-		@Override
-		protected Bitmap doInBackground(String... arg) {
-			try {
-				byte[] result = HttpUtils.get(arg[0]);
-				return BitmapFactory.decodeByteArray(result, 0, result.length);
-			} catch (IOException e) {
-				return null;
-			}
-		}
-
-		@Override
-		protected void onPostExecute(Bitmap bitmap) {
-			View rowView=null; //TODO tolta temporaneamente var globale statica
-			if (bitmap != null) {
-				ImageView pictureView = (ImageView) rowView.findViewById(R.id.imgIcon);
-				pictureView.setImageBitmap(bitmap);
-			}
-		}
-	}
-
-	private static class ViewHolder {
-		ImageView imageView;
-		Bitmap bitmap;
-		String imageURL;
-	}
-
-
-	private class DownloadAsyncTask extends AsyncTask<ViewHolder, Void, ViewHolder> {
-		@Override
-		protected ViewHolder doInBackground(ViewHolder... params) {
-			//load image directly
-			ViewHolder viewHolder = params[0];
-			try {
-				URL imageURL = new URL(viewHolder.imageURL);
-				viewHolder.bitmap = BitmapFactory.decodeStream(imageURL.openStream());
-			} catch (IOException e) {
-				Log.e("error", "Downloading Image Failed");
-				viewHolder.bitmap = null;
-			}
-
-			return viewHolder;
-		}
-
-		@Override
-		protected void onPostExecute(ViewHolder result) {
-			if (result.bitmap != null)
-				result.imageView.setImageBitmap(result.bitmap);
-		}
 	}
 }
 
